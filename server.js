5683b2b6204937b94de06c36f806ee0e5c8788a0
reisvictor1 <reisvictor448@gmail.com>
2020-05-17 19:07:08 -0300

    Atualizações

--- server.js
+++ server.js
@@ -5,18 +5,26 @@
 
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 4000
 
 const users = []
+let loggedUser; 
 
 const app = express()
 
+app.use(cors())
 app.use(bodyparser.json())
 
 
 app.post('/register', (req, res) => {
 
     const { name, password, address, email } = req.body;
+
+    users.forEach((user) => {
+        if(user.username == user) res.status(400).send('Já existe um usuário com este nome')
+    })
+
 
     if(name.match(' ')) return res.status(400).send('O nome de usuário não pode conter espaços')
     if(name.match('[A-Z]')) return res.status(400).send('O nome de usuário não pode conter caracteres maiúsculos')
@@ -35,19 +43,21 @@ app.post('/register', (req, res) => {
 
     users.push(user)
 
-    console.log(users)
-
     return res.status(200).send('Usuário Cadastrado')
 })
 
+app.get('/user', (req,res) => {
+    return res.status(200).json(loggedUser)
+})
 
 app.post('/login', async (req, res) => {
 
     const { name, pass } = req.body;
 
-    users.forEach((user)=>{
+    users.forEach((user)=>{ 
         if(user.username == name && user.password != pass) return res.status(400).send(`Senha incorreta`)
         if(user.username == name && user.password == pass) {
+            loggedUser = user;
             return res.status(200).json(user)
         }
     })
@@ -57,9 +67,9 @@ app.post('/login', async (req, res) => {
 })
 
 
-app.delete('/:username', (req, res) => {
+app.delete('/user', (req, res) => {
 
-    const { username } = req.params
+    const { username } = req.body
 
     let index = -1;
 
@@ -67,12 +77,12 @@ app.delete('/:username', (req, res) => {
         console.log(user)
         index++
         if(user.username == username){
+            loggedUser = '';
             users.splice(index, 1)
             res.status(200).send('Usuário Removido')
             return
         }
     })
-    
 
     return res.status(400).send('Usuário não encontrado')
 })
